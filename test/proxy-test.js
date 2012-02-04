7e245c0d440d590643ac0a226e59dd97e0a7e772
Andreas Lind Petersen <andreas@one.com>
2012-02-04 21:25:38 +0100

    Added simple proxy-with-mapping test.

--- test/proxy-test.js
+++ test/proxy-test.js
@@ -35,5 +35,32 @@ vows.describe('foo').addBatch({
             assert.isNull(err);
             assert.matches(body, /socket\.io/);
         }
+    },
+    'create a livestyle server with a mapping from /foo/ to /bar/ along with an upstream server, then request /foo/hello.txt': {
+        topic: function () {
+            var callback = this.callback,
+                documentRoot = path.resolve(__dirname, 'proxy'),
+                upstreamServer = express.createServer()
+                    .use(express['static'](documentRoot));
+
+            upstreamServer.listen(0);
+
+            var upstreamServerUrl = 'http://127.0.0.1:' + upstreamServer.address().port + '/',
+                appInfo = createLiveStyleTestServer({
+                    proxy: upstreamServerUrl,
+                    mappings: {
+                        '/foo/': '/bar/'
+                    }
+                });
+
+            // Wait a couple of seconds for the servers to become available
+            setTimeout(function () {
+                request('http://127.0.0.1:' + appInfo.port + '/foo/hello.txt', callback);
+            }, 2000);
+        },
+        'the contents of /bar/hello.txt should be served': function (err, response, body) {
+            assert.isNull(err);
+            assert.equal(body, 'The contents of /bar/hello.txt\n');
+        }
     }
 })['export'](module);
